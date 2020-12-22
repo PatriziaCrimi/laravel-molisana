3773b0a52b54b42274d52ccff9cb2f1a911099ad
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-12-22 15:32:04 +0100

    Initialization of Project & basic structure set up

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>La Molisana: pasta semole e farine di qualità</title>
+    <link rel="icon" type="img/png" href="{{asset('img/favicon.png')}}">
+  </head>
+  <body>
+    <h1>Prova Home</h1>
+    <a href="{{route('products-page')}}">Link Products prova</a>
+  </body>
+</html>
