fc6ed7d6f503df99a8daae702077ff1c814c4ec0
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-12-22 17:21:31 +0100

    Layout setup - creation of "layouts" sub-folder in "views"

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,66 +1,9 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>La Molisana: pasta semole e farine di qualità</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="img/png" href="{{asset('img/favicon.png')}}">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-  </head>
-  <body>
-    <div id="page-wrapper">
-      {{-- Header --}}
-      <header>
-        <div class="container">
-          <div class="row">
-            <div class="col">
-              <div class="logo-header text-center">
-                <img src="{{asset('img/la-molisana-logo.png')}}" alt="La Molisana logo">
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col">
-              {{-- Navbar --}}
-              <nav>
-                <ul class="nav-menu text-center">
-                  <li>
-                    <a href="{{url('/')}}">Home</a>
-                  </li>
-                  <li>
-                    <a href="{{route('products-page')}}">Prodotti</a>
-                  </li>
-                  <li>
-                    <a href="{{route('news-page')}}">News</a>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-          </div>
-        </div>  {{-- Closing Header container --}}
-      </header>
-      {{-- Main --}}
-      <main>
-        <div class="container">
-          <div class="row">
-            <div class="col">
-              PROVA MAIN
-            </div>
-          </div>
-        </div>  {{-- Closing Main container --}}
-      </main>
-      {{-- Footer --}}
-      <footer>
-        <div class="container">
-          <div class="row">
-            <div class="col">
-              prova footer
-            </div>
-          </div>
-        </div>
-      </footer>
-    </div>  {{-- Closing page-wrapper --}}
-  </body>
-</html>
+@extends('layouts.app')
+
+@section('section')
+  <section class="text-center">
+    <h1>Prova Home Main-Section</h1>
+  </section>
+@endsection
+
+{{-- <title>La Molisana: pasta semole e farine di qualità</title> --}}
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>I prodotti | La Molisana</title>
-    <link rel="icon" type="img/png" href="{{asset('img/favicon.png')}}">
-  </head>
-  <body>
-    <h1>Prova Products</h1>
-    <a href="{{url('/')}}">Link Home prova</a>
-  </body>
-</html>
+@extends('layouts.app')
+
+@section('section')
+  <section class="text-center">
+    <h1>Prova Products Main-Section</h1>
+  </section>
+@endsection
+
+{{-- <title>I prodotti | La Molisana</title> --}}
