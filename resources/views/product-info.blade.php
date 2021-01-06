e95002da75c8380854fce3a6158373685b613e92
PatriziaCrimi <crimi.patrizia@gmail.com>
2021-01-06 18:02:28 +0100

    "Product-info" page completed - responsive mobile-first

--- resources/views/product-info.blade.php
+++ resources/views/product-info.blade.php
@@ -10,7 +10,7 @@
           <h1>{{ $product['titolo'] }}</h1>
         </div>
       </div>
-    </div>  {{-- Closing Container --}}
+    </div>  {{-- Closing Container-fluid --}}
     {{-- Pictures --}}
     <div class="product-pic">
       <img src="{{ $product['src-h'] }}" alt="{{ $product['titolo'] }}">
@@ -19,17 +19,42 @@
       <img src="{{ $product['src-p'] }}" alt="{{ $product['titolo'] . 'package'}} package">
     </div>
     {{-- Product Details --}}
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-12">
-          <div class="product-details">
-            <p>{{ $product['tipo'] }}</p>
-            <p>{{ $product['cottura'] }}</p>
-            <p>{{ $product['peso'] }}</p>
-            <p class="text-justify">{!! $product['descrizione'] !!}</p>
+    <div class="container">
+      <div class="product-details">
+        <div class="row">
+          <div class="col-12 col-md-4">
+            <div class="info-box">
+              <i class="fas fa-2x fa-info"></i>
+              <p>Formato:
+                <span>{{ $product['tipo'] }}</span>
+              </p>
+            </div>
+          </div>
+          <div class="col-12 col-md-4">
+            <div class="info-box">
+              <i class="far fa-2x fa-clock"></i>
+              <p>Tempo di cottura:
+                <span>{{ $product['cottura'] }}</span>
+              </p>
+            </div>
+          </div>
+          <div class="col-12 col-md-4">
+            <div class="info-box weight">
+              <i class="fas fa-2x fa-weight-hanging"></i>
+              <p>Peso:
+                <span>{{ $product['peso'] }}</span>
+              </p>
+            </div>
           </div>
         </div>
       </div>
     </div>  {{-- Closing Container --}}
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <p class="description text-justify">{!! $product['descrizione'] !!}</p>
+        </div>
+      </div>
+    </div>  {{-- Closing Container-fluid --}}
   </section>
 @endsection
